16ff84c54450d2cf56dd8b5123376521271aaf0a
Dark Knight <jordanccall@gmail.com>
2020-09-15 13:38:22 -0600

    fixed server

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(routes);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({ force: false }).then(function() {
   app.listen(PORT, function() {
     console.log(`🌎  ==> Server now listening on PORT ${PORT}!`);
     seeder.up(db.Products);
