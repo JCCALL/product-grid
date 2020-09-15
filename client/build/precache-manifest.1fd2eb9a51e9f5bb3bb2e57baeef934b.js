02082ed9bb5845a1fcf15193f10aa289c377cc23
Dark Knight <jordanccall@gmail.com>
2020-09-15 15:21:14 -0600

    add build

---
+++ client/build/precache-manifest.1fd2eb9a51e9f5bb3bb2e57baeef934b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74cbbf83b909a212fd84ed6db59417ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb755db6d7b8ef122b47",
+    "url": "/static/css/main.6055321d.chunk.css"
+  },
+  {
+    "revision": "53823ec79af4a64fef18",
+    "url": "/static/js/2.69843a96.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.69843a96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb755db6d7b8ef122b47",
+    "url": "/static/js/main.6775b0eb.chunk.js"
+  },
+  {
+    "revision": "e3a7b6e129db16b36836",
+    "url": "/static/js/runtime-main.8e1094cd.js"
+  }
+]);
